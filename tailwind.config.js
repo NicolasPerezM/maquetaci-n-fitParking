34ccc3e450f440482b65e4e2bc58d8158e176e3a
Nicolas Perez <nbome12@outlook.com>
2024-05-29 13:48:16 -0500

    inicio de sesion primer commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'general': "url('/images/background.jpeg')"
+        'general': "url('/images/background.png')",
+        'login-img': "url('/images/login-imageBackground.jpeg')"
+      },
+      colors: {
+        'main-yellow': '#F2C40D',
+        'main-black': '#0D0D0D'
       }
     },
   },
